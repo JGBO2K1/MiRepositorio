9f6771b4765416568d558ec720f98668d763f901
Juan Antonio <jgonzalezbolona@gmail.com>
2022-11-17 14:45:15 +0100

    He creado un archivo.java

---
+++ EjercicioCodd.java
@@ -0,0 +1,19 @@
+
+
+
+
+
+package ejerciciocodd;
+
+public class EjercicioCodd {
+
+    
+    public static void main(String[] args) {
+       
+   int base=5;
+   int altura=6;
+   int resultado= base*altura;
+   System.out.println(" El área de un triángulo es " + resultado);
+    }
+    
+}
