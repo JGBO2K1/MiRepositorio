eb76ca6f16af3cb9b130a94562df0c1efad36fe9
Juan Antonio <jgonzalezbolona@gmail.com>
2022-11-17 14:20:44 +0100

    añado ejercicios.java

---
+++ Ejerciciocod.java
@@ -0,0 +1,20 @@
+
+package ejerciciocod;
+
+public class Ejerciciocod {
+
+ 
+    public static void main(String[] args) {
+        
+        
+        
+       System.out.println("Hola mis estimados compañeros");
+        
+        
+        
+        
+        
+    
+    }
+    
+}
